fef53a799cdef74b8b1df8b6bfa7971c13648dff
joohokim <give928@gmail.com>
2022-06-19 15:26:39 +0900

    test: 상품 Business Object Test, Controller Test

---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,83 @@
+package kitchenpos.ui;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kitchenpos.annotation.MockMvcEncodingConfiguration;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@MockMvcEncodingConfiguration
+@WebMvcTest(ProductRestController.class)
+class ProductRestControllerTest {
+    private static final String URL = "/api/products";
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    private Product product1;
+
+    @BeforeEach
+    void setUp() {
+        product1 = new Product(1L, "음식1", BigDecimal.ONE);
+    }
+
+    @DisplayName("상품을 등록하고 등록한 상품을 반환한다.")
+    @Test
+    void create() throws Exception {
+        // given
+        Product product = new Product(product1.getName(), product1.getPrice());
+
+        given(productService.create(product)).willReturn(product1);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(URL)
+                                                              .contentType(MediaType.APPLICATION_JSON)
+                                                              .content(objectMapper.writeValueAsBytes(product)))
+                .andDo(print());
+
+        // then
+        resultActions.andExpect(status().isCreated())
+                .andExpect(content().string(objectMapper.writeValueAsString(product1)));
+    }
+
+    @DisplayName("상품의 전체 목록을 조회한다.")
+    @Test
+    void list() throws Exception {
+        // given
+        List<Product> products = Arrays.asList(product1, new Product(2L, "음식2", BigDecimal.ONE));
+
+        given(productService.list()).willReturn(products);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(URL))
+                .andDo(print());
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(products)));
+    }
+}
