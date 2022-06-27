d206d7fe92779d6499e7b754ed1cadbaaa8bdc8d
joohokim <give928@gmail.com>
2022-06-28 00:22:30 +0900

    test: 아키텍처 테스트

--- src/test/java/kitchenpos/dependency/LayeredArchitectureRuleTest.java
+++ src/test/java/kitchenpos/dependency/LayeredArchitectureRuleTest.java
@@ -1,5 +1,6 @@
 package kitchenpos.dependency;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
@@ -7,14 +8,16 @@ import org.junit.jupiter.api.DisplayName;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
-@AnalyzeClasses(packages = "kitchenpos")
+@AnalyzeClasses(packages = "kitchenpos", importOptions = {ImportOption.DoNotIncludeTests.class})
 @DisplayName("레이어드 아키텍처 의존성 테스트")
 class LayeredArchitectureRuleTest {
     /**
      * ui 레이어는 아무 레이어에서도 접근할 수 없다.
      * application 레이어는 ui, application 레이어에서만 접근할 수 있다.
-     * domain 레이어는 application, dto, 인수 테스트 레이어에서만 접근할 수 있다.
-     * dto 레이어는 ui, application, 인수 테스트 레이어에서만 접근할 수 있다.
+     * domain 레이어는 application, dto 레이어에서만 접근할 수 있다.
+     * dto 레이어는 ui, application 레이어에서만 접근할 수 있다.
+     *
+     * ui -> application -> domain
      */
     @ArchTest
     ArchRule 레이어드_아키텍처_규칙 = layeredArchitecture()
@@ -22,9 +25,8 @@ class LayeredArchitectureRuleTest {
             .layer("application").definedBy("..application..")
             .layer("domain").definedBy("..domain..")
             .layer("dto").definedBy("..dto..")
-            .layer("acceptance").definedBy("..acceptance..")
             .whereLayer("ui").mayNotBeAccessedByAnyLayer()
             .whereLayer("application").mayOnlyBeAccessedByLayers("ui", "application")
-            .whereLayer("domain").mayOnlyBeAccessedByLayers("application", "dto", "acceptance")
-            .whereLayer("dto").mayOnlyBeAccessedByLayers("ui", "application", "acceptance");
+            .whereLayer("domain").mayOnlyBeAccessedByLayers("application", "dto")
+            .whereLayer("dto").mayOnlyBeAccessedByLayers("ui", "application");
 }
